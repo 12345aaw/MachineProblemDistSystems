1f9fc059bad3c6329c47124f4edca5bb134431d1
12345aaw <willis.andrew.a@gmail.com>
2018-03-19 23:56:39 -0400

    start of basic multicast

---
+++ BasicMulticast.py
@@ -0,0 +1,9 @@
+from MulticastAbstract import MulticastAbstract
+import unicast as u
+class BasicMulticast( MulticastAbstract ):
+    RECEIVED = []
+    def multicast(self, group, message):
+        for i in group:
+            u.unicast_send(group[i],message)
+    def deliver(self, source,message):
+        u.receive()
